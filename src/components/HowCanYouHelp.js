89f7cae28b795ac4f8187a7375f44861f5821c12
1Shubham7 <shubhammahar1306@gmail.com>
2023-06-08 13:30:16 +0530

    basic website ready

---
+++ src/components/HowCanYouHelp.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function HowCanYouHelp() {
+
+
+    const wifestyle2 = {
+        margin: '0',
+        color: 'white',
+        background:'green',
+        position:'relative',
+        display:'flex',
+        flexDirection: 'column',
+        alignItems:'center',
+        padding: "100px 20vw",
+        minHeight: '400px',
+        // fontFamily:
+      }
+
+  return (
+    <div>
+      
+      
+      <section style={wifestyle2} >
+        <h1 style={{textDecoration:'underline'}}>How can you help!</h1>
+        <p>To expand the database we need more pictures. I will be more than happy to add up more images to the database, kindly send me links of images or the image itself on my <a style={{color:'yellow'}} href='shubhammahar1306@gmail.com'>email</a> or <a style={{color:'yellow'}} href="https://www.linkedin.com/in/shubham-singh-mahar-5a002b20b/">LinkedIn</a>. The code of this website is also open-source (avaible on GitHub and GitLab), you can add modifications as per your wish. </p>
+        </section>
+    </div>
+  )
+}
