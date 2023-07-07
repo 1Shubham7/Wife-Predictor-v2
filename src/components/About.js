c512091fa3ac82e893be6683572635c85d25db6b
theQuery <tomkri2000@outlook.com>
2023-07-08 00:34:10 +0200

    Fix grammatical errors in the app

--- src/components/About.js
+++ src/components/About.js
@@ -4,11 +4,11 @@ import './photo.css'
 export default function About() {
   const wifestyle2 = {
     margin: '0',
-    background:'yellow',
-    position:'relative',
-    display:'flex',
+    background: 'yellow',
+    position: 'relative',
+    display: 'flex',
     flexDirection: 'column',
-    alignItems:'center',
+    alignItems: 'center',
     padding: "100px 20vw",
     minHeight: '200px',
     color: 'green'
@@ -17,40 +17,41 @@ export default function About() {
 
   return (
     <>
-    <div  className="superClass" style={{backgroundColor: 'yellow', color: 'green', paddingTop:'40px', paddingBottom:'40px'}}>
-    
-    <div className="circular-image" >
-    <img
-        src="CVmin.jpg"
-        style={{
-          width: "300px",
-          height: "300px",
-          borderRadius: "50%",
-          overflow: "hidden",
-          border: "5px solid green",
-          boxSizing: "border-box",
-          animation: "border-pulse 2s infinite"
-          
-          
-          
-        }}
-        alt="Shubham Singh"
-      />
+      <div className="superClass" style={{ backgroundColor: 'yellow', color: 'green', paddingTop: '40px', paddingBottom: '40px' }}>
 
-    </div >
-    <div className="text" style={{textAlign: 'center', justifyContent:'center'}}>
+        <div className="circular-image" >
+          <img
+            src="CVmin.jpg"
+            style={{
+              width: "300px",
+              height: "300px",
+              borderRadius: "50%",
+              overflow: "hidden",
+              border: "5px solid green",
+              boxSizing: "border-box",
+              animation: "border-pulse 2s infinite"
 
-      <section style={wifestyle2} >
-        <h1 style={{textDecoration:'underline'}}>About Me</h1>
-        <p>  This is Shubham Singh Mahar, I am First Year Undergrad persuing Bachelors in Technology with Computer Science Eng. major from a tier-3 college in Ghaziabad.
-        I am from Haldwani, Nanital, Uttarakhand.
-        The code is available in this github reposetory <a href="https://github.com/1Shubham7/Wife-Predictor">Wife-Predictor</a>. 
-         Staring the repo on GitHub would be really helpful!</p>
-        <p>Do checkout my blog <a href="https://1shubham7.hashnode.dev/">1Shubham7</a></p>
-      </section>
-    </div>
-    </div>
-    
+
+
+            }}
+            alt="Shubham Singh"
+          />
+
+        </div >
+        <div className="text" style={{ textAlign: 'center', justifyContent: 'center' }}>
+
+          <section style={wifestyle2} >
+            <h1 style={{ textDecoration: 'underline' }}>About Me</h1>
+            <p>This is Shubham Singh Mahar. I am a First Year Undergrad pursuing
+              Bachelors in Technology with Computer Science Eng. major from a tier-3 college in Ghaziabad.
+              I am from Haldwani, Nanital, Uttarakhand.
+              The code for this project is available in the GitHub repository <a href="https://github.com/1Shubham7/Wife-Predictor">Wife-Predictor</a>.
+              Starring the repository on GitHub would be really helpful!</p>
+            <p>Do check out my blog <a href="https://1shubham7.hashnode.dev/">1Shubham7</a>.</p>
+          </section>
+        </div>
+      </div>
+
     </>
   );
 }
--- src/components/HowCanYouHelp.js
+++ src/components/HowCanYouHelp.js
@@ -3,27 +3,25 @@ import React from 'react'
 export default function HowCanYouHelp() {
 
 
-    const wifestyle2 = {
-        margin: '0',
-        color: 'white',
-        background:'green',
-        position:'relative',
-        display:'flex',
-        flexDirection: 'column',
-        alignItems:'center',
-        padding: "100px 20vw",
-        minHeight: '400px',
-        // fontFamily:
-      }
+  const wifestyle2 = {
+    margin: '0',
+    color: 'white',
+    background: 'green',
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: "100px 20vw",
+    minHeight: '400px',
+    // fontFamily:
+  }
 
   return (
     <div>
-      
-      
       <section style={wifestyle2} >
-        <h1 style={{textDecoration:'underline'}}>How can you help!</h1>
-        <p>To expand the database we need more pictures. I will be more than happy to add up more images to the database, kindly send me links of images or the image itself on my <a style={{color:'yellow'}} href='shubhammahar1306@gmail.com'>email</a> or <a style={{color:'yellow'}} href="https://www.linkedin.com/in/shubham-singh-mahar-5a002b20b/">LinkedIn</a>. The code of this website is also open-source (avaible on GitHub and GitLab), you can add modifications as per your wish. </p>
-        </section>
+        <h1 style={{ textDecoration: 'underline' }}>How can you help?</h1>
+        <p>To expand the database, we need more images. I will be more than happy to add suggested images to the database. Kindly send me the images or their corresponding links to my <a style={{ color: 'yellow' }} href='shubhammahar1306@gmail.com'>email</a> or <a style={{ color: 'yellow' }} href="https://www.linkedin.com/in/shubham-singh-mahar-5a002b20b/">LinkedIn</a>. The codebase of this project is also open-source (on GitHub and GitLab), so you can add modifications as you wish.</p>
+      </section>
     </div>
   )
 }
