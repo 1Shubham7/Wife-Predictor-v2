26ba303ba9ca155cfcb367a94cc86e1b3e1e29cf
1Shubham7 <shubhammahar1306@gmail.com>
2023-07-07 18:41:21 +0530

    Finished up with the app.js all issues fixed

--- src/App.js
+++ src/App.js
@@ -1,69 +1,76 @@
-
-import './App.css';
-import Wife from './components/Wife';
-import Footer from './components/Footer';
-import Confirm from './components/Confirm'
-import HowCanYouHelp from './components/HowCanYouHelp';
-import './components/My.css'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-import About from './components/About'
-
-
+import "./App.css";
+import Wife from "./components/Wife";
+import Footer from "./components/Footer";
+import Confirm from "./components/Confirm";
+import HowCanYouHelp from "./components/HowCanYouHelp";
+import "./components/My.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import About from "./components/About";
 
 function App() {
-
-
-const refreshAboutPage = ()=>{
-  setTimeout(() => {
-    window.location.reload()
-  }, 50);
-
-}
-
-
+  const refreshAboutPage = () => {
+    setTimeout(() => {
+      window.location.reload();
+    }, 50);
+  };
 
   return (
     <div className="App">
       <Router>
-      
-    
-  <div className="spacer layer1"></div>
-      <nav className='mainnav'>
-        <ul>
-          
-            
-        {/* <a  href={window.location.pathname==='/wife-predictor/about'? '/wife-predictor' : '#wifeplay'} style={{color:'green'}}>
-        <li className="navclass news " id="navbarid" onClick={window.location.pathname==='/wife-predictor/about'?refreshAboutPage:null}>{window.location.pathname==='/wife-predictor' ? 'Play' : 'Home'}</li>
-      </a> */}
+        <div className="spacer layer1"></div>
+        <nav className="mainnav">
+          <ul>
+            <a
+              href={window.location.pathname === "/about" ? "/" : "#wifeplay"}
+              style={{ color: "green" }}
+            >
+              <li
+                className="navclass news "
+                id="navbarid"
+                onClick={
+                  window.location.pathname === "/about"
+                    ? refreshAboutPage
+                    : null
+                }
+              >
+                {window.location.pathname === "/" ? "Play" : "Home"}
+              </li>
+            </a>
 
-<a  href={window.location.pathname==='/about'? '/' : '#wifeplay'} style={{color:'green'}}>
-        <li className="navclass news " id="navbarid" onClick={window.location.pathname==='/about'?refreshAboutPage:null}>{window.location.pathname==='/' ? 'Play' : 'Home'}</li>
-      </a>
+            <a
+              href="https://github.com/1Shubham7"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <li className="news">GitHub</li>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/shubham-singh-mahar-5a002b20b/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <li className="news">LinkedIn</li>
+            </a>
+            <Link to="/about" onClick={refreshAboutPage}>
+              <li className="news">About</li>
+            </Link>
+          </ul>
+        </nav>
 
-          <a href="https://github.com/1Shubham7" target="_blank" rel="noreferrer"><li  className="news">GitHub</li></a>
-          <a href="https://www.linkedin.com/in/shubham-singh-mahar-5a002b20b/" target="_blank" rel="noreferrer" ><li  className="news">LinkedIn</li></a>
-          <Link to="/about" onClick={refreshAboutPage}><li  className="news">About</li></Link>
-          
-        </ul>
-      </nav>
-      
-
-      <Routes>
-  <Route path="/about" element={<About />} />
-    <Route path='/' element={<>
-    <Wife />
-      <Confirm />
-      <HowCanYouHelp />
-      </>} />
-
-</Routes>
-      <Footer />
-      
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Wife />
+                <Confirm />
+                <HowCanYouHelp />
+              </>
+            }
+          />
+        </Routes>
+        <Footer />
       </Router>
     </div>
   );
