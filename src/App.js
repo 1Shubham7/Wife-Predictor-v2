f11097f2d90c31fe6a383d635eaf3558539fa151
yash-tyagi-2003 <141833519+yash-tyagi-2003@users.noreply.github.com>
2024-05-11 01:29:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,66 +3,28 @@ import Wife from "./components/Wife";
 import Footer from "./components/Footer";
 import Confirm from "./components/Confirm";
 import HowCanYouHelp from "./components/HowCanYouHelp";
+import Navbar from "./components/Navbar";
 import "./components/My.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import About from "./components/About";
 
 function App() {
-  const refreshAboutPage = () => {
-    setTimeout(() => {
-      window.location.reload();
-    }, 50);
-  };
+
 
   return (
     <div className="App">
       <Router>
-        <div className="spacer layer1"></div>
-        <nav className="mainnav">
-          <ul>
-            <a
-              href={window.location.pathname === "/about" ? "/" : "#wifeplay"}
-              style={{ color: "green" }}
-            >
-              <li
-                className="navclass news "
-                id="navbarid"
-                onClick={
-                  window.location.pathname === "/about"
-                    ? refreshAboutPage
-                    : null
-                }
-              >
-                {window.location.pathname === "/" ? "Play" : "Home"}
-              </li>
-            </a>
-
-            <a
-              href="https://github.com/1Shubham7"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <li className="news">GitHub</li>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/shubham-singh-mahar-5a002b20b/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <li className="news">LinkedIn</li>
-            </a>
-            <Link to="/about" onClick={refreshAboutPage}>
-              <li className="news">About</li>
-            </Link>
-          </ul>
-        </nav>
-
         <Routes>
-          <Route path="/about" element={<About />} />
+          <Route path="/about" element={<>
+          <Navbar/>
+          <About/>
+          </>} />
           <Route
             path="/"
             element={
               <>
+                <Navbar/>
                 <Wife />
                 <Confirm />
                 <HowCanYouHelp />
